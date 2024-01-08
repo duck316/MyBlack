b3bb96b177e05473bf617941fa39503536631d3b
duck316 <duck316@gmail.com>
2024-01-08 05:59:32 -0500

    App MyBlack, Muestra información de la compañia, metodos de pago entre otras informaciones

---
+++ app/src/main/java/com/alt/myblack/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.alt.myblack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
